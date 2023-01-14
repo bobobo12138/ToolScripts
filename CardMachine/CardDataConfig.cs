8e10fbe68c4fa7c5a7d7e5431b27b33a6fe9144e
bobobo12138 <88996212+bobobo12138@users.noreply.github.com>
2023-01-14 17:59:19 +0800

    未完成的抽卡机
    未完成的抽卡机

---
+++ CardMachine/CardDataConfig.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class SingleCardData
+{
+    [Header("唯一ID")]
+    public int ID;
+    [Header("名字")]
+    public string name;
+    [Header("卡片类型")]
+    public CardTyple cardTyple;
+    [Header("默认概率权重，0-100")]
+    [Range(0, 100)]
+    public float default_P_Weight;
+    [Header("几率权重曲线，Y为输出，X为输入")]
+    public AnimationCurve animationCurve_P_Weight;
+    ///某个抽象类，代表抽到的物品
+    ///public xxx
+
+
+
+
+    /// <summary>
+    /// 从曲线获得概率
+    /// </summary>
+    /// <param name="_x">传入x轴值，返回曲线的Y</param>
+    public float GetPFromCurve(float _x)
+    {
+        if (_x > 1 || _x < 0)
+        {
+            Debug.Log("传入x错误");
+            return 0;
+        }
+
+        return animationCurve_P_Weight.Evaluate(_x);
+    }
+}
+
+
+public enum CardTyple
+{ 
+    none,
+    Normal,
+    Weapon,
+}
+
+[CreateAssetMenu(menuName = "Config/CardDataConfig")]
+public class CardDataConfig : ScriptableObject
+{
+    public List<SingleCardData> cardDatas = new List<SingleCardData>();
+
+}
+
---
+++ CardMachine/CardMachine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CardMachine : MonoBehaviour
+{
+    public List<SingleCardData> cardDatas = new List<SingleCardData>();
+
+
+
+    public LinkedList<SingleCardData> GetCard(int num)
+    {
+        LinkedList<SingleCardData> selectedData = new LinkedList<SingleCardData>();
+
+
+
+    }
+}
