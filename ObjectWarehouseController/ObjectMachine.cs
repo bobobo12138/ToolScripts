974248bc2967d0575e2bf169f7bbb2c1e9e32262
bobobo12138 <88996212+bobobo12138@users.noreply.github.com>
2023-01-30 22:24:01 +0800

    对象仓库
    对象仓库

---
+++ ObjectWarehouseController/ObjectMachine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectMachine
+{
+    public int machineId;
+
+
+
+    public void Update()
+    {
+
+    }
+}
