d503a4eb487b32fabe6c138c17286c0dc7dc02b5
DESKTOP-AG8T4PJ\Administrator <13649032100@163.com>
2023-06-03 18:38:59 +0800

    子窗口示例

---
+++ General/Script/GChildPanel/ChildPanel_Demo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildPanel_Demo : ChildPanelBase
+{
+    protected override void OnInit()
+    {
+        //需要手动设置uIFollowMouse的数据
+        //uIFollowMouse.SetData
+    }
+
+    protected override void OnRefresh()
+    {
+
+    }
+}
