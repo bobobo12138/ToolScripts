009779fe5d52c8e0cccb69c814d02b665f60bd8b
DESKTOP-AG8T4PJ\Administrator <13649032100@163.com>
2023-06-07 11:18:05 +0800

    UI移动组件只在播放时添加事件

--- General/Script/Animation_UITranslate.cs
+++ General/Script/Animation_UITranslate.cs
@@ -14,17 +14,19 @@ public class Animation_UITranslate
 {
     public RectTransform rectTransform;
 
-    public Action beforePlay;
-    public Action afterPlay;
+    //public Action beforePlay;
+    //public Action afterPlay;
 
-    public Action beforePlayBack;
-    public Action afterPlayBack;
+    //public Action beforePlayBack;
+    //public Action afterPlayBack;
 
     /// <summary>自定义方向</summary>
     public Vector2 dir;
 
     /// <summary>是否仅在active状态下播放</summary>
-    public bool playOnlyInActive=false;
+    public bool playOnlyInActive = false;
+    public float duration = 0.5f;
+
     public Animation_UITranslate(RectTransform rectTransform)
     {
         this.rectTransform = rectTransform;
@@ -48,10 +50,30 @@ public class Animation_UITranslate
             if (!rectTransform.gameObject.activeSelf) return;
         }
 
-        beforePlay?.Invoke();
+        //beforePlay?.Invoke();
         rectTransform.anchoredPosition = new Vector2(rectTransform.rect.width, rectTransform.rect.height) * dir;
-        rectTransform.DOAnchorPos(Vector2.zero, 0.5f).OnComplete(() => { afterPlay?.Invoke(); });
+        rectTransform.DOAnchorPos(Vector2.zero, duration).OnComplete(() =>
+        {
+            //afterPlay?.Invoke();
+        });
     }
+
+    public void Play(Action before, Action after)
+    {
+        if (playOnlyInActive)
+        {
+            if (!rectTransform.gameObject.activeSelf) return;
+        }
+
+        //beforePlay?.Invoke();
+        before?.Invoke();
+        rectTransform.anchoredPosition = new Vector2(rectTransform.rect.width, rectTransform.rect.height) * dir;
+        rectTransform.DOAnchorPos(Vector2.zero, duration).OnComplete(() =>
+        {
+            //afterPlay?.Invoke(); after?.Invoke();
+        });
+    }
+
 
     /// <summary>
     /// 回放
@@ -63,7 +85,25 @@ public class Animation_UITranslate
             if (!rectTransform.gameObject.activeSelf) return;
         }
 
-        beforePlayBack?.Invoke();
-        rectTransform.DOAnchorPos(new Vector2(rectTransform.rect.width, rectTransform.rect.height) * dir, 0.5f).OnComplete(() => { afterPlayBack?.Invoke(); });
+        //beforePlayBack?.Invoke();
+        rectTransform.DOAnchorPos(new Vector2(rectTransform.rect.width, rectTransform.rect.height) * dir, duration).OnComplete(() =>
+        {
+            //afterPlayBack?.Invoke();
+        });
+    }
+
+    public void PlayBack(Action before, Action after)
+    {
+        if (playOnlyInActive)
+        {
+            if (!rectTransform.gameObject.activeSelf) return;
+        }
+
+        //beforePlayBack?.Invoke();
+        before?.Invoke();
+        rectTransform.DOAnchorPos(new Vector2(rectTransform.rect.width, rectTransform.rect.height) * dir, duration).OnComplete(() =>
+        {
+            //afterPlayBack?.Invoke(); after?.Invoke();
+        });
     }
 }
