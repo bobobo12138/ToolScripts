cd964aabb0ddee6fb99307832a27b11c59477cba
admin_AIJia <13649032100@163.com>
2023-08-19 23:10:14 +0800

    整理文件夹，准备重新制作第一人称控制器

--- General/Script/PlayerControllers/RayController/Script/RayController.cs
+++ General/Script/PlayerControllers/RayController/Script/RayController.cs
@@ -1,66 +1,67 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 using UnityEngine.InputSystem;
-
-public class RayController : MonoBehaviour
+namespace General
 {
-    /// 初始化参数
-    [HideInInspector]
-    public bool awakeInit = true;
-
-    bool _isInit = false;
-    public bool isInit { get { return _isInit; } private set { _isInit = value; } }
-
-    [SerializeField]
-    [Header("主要参数")]
-    public int maxDistance = 100;
-    public LayerMask layerMask;
-    public Action<Vector3> onClick;
-
-    RayInputSystem rayInputSystem;
-
-    private void Awake()
+    public class RayController : MonoBehaviour
     {
-        if (awakeInit) Init();
-    }
-    public void Init()
-    {
-        if (isInit) return;
-        rayInputSystem = new RayInputSystem();
-        rayInputSystem.Enable();
-        rayInputSystem.Player.RayButton.performed += OnPerformedRay;
-        isInit = true;
-    }
+        /// 初始化参数
+        [HideInInspector]
+        public bool awakeInit = true;
 
+        bool _isInit = false;
+        public bool isInit { get { return _isInit; } private set { _isInit = value; } }
 
+        [SerializeField]
+        [Header("主要参数")]
+        public int maxDistance = 100;
+        public LayerMask layerMask;
+        public Action<Vector3> onClick;
 
-    public void Enable()
-    {
-        rayInputSystem.Enable();
-    }
+        RayInputSystem rayInputSystem;
 
-
-    public void Disable()
-    {
-        rayInputSystem.Disable();
-    }
-
-
-    /// <summary>
-    /// rayInputSystem.raybutton回调坐标时
-    /// </summary>
-    /// <param name="data"></param>
-    void OnPerformedRay(CallbackContext data)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, maxDistance, layerMask))
+        private void Awake()
         {
-            Debug.Log(hit.collider.name);
-            onClick(hit.point);
+            if (awakeInit) Init();
+        }
+        public void Init()
+        {
+            if (isInit) return;
+            rayInputSystem = new RayInputSystem();
+            rayInputSystem.Enable();
+            rayInputSystem.Player.RayButton.performed += OnPerformedRay;
+            isInit = true;
+        }
+
+
+
+        public void Enable()
+        {
+            rayInputSystem.Enable();
+        }
+
+
+        public void Disable()
+        {
+            rayInputSystem.Disable();
+        }
+
+
+        /// <summary>
+        /// rayInputSystem.raybutton回调坐标时
+        /// </summary>
+        /// <param name="data"></param>
+        void OnPerformedRay(CallbackContext data)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, maxDistance, layerMask))
+            {
+                Debug.Log(hit.collider.name);
+                onClick(hit.point);
+            }
         }
     }
+
 }
